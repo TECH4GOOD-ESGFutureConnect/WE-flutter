900f435896ae33cf384a1c7c74dcdb56b1b3b41a
sangjun-Park1208 <77184523+sangjun-Park1208@users.noreply.github.com>
2023-09-08 07:02:36 +0900

    feat: 지하철 내부 지도 완성

--- src/page/SubwayPage/index.tsx
+++ src/page/SubwayPage/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
 import * as s from "./styles";
 import Header from '../../component/Common/Header';
+import subway from "../../image/Subway/Subway.png";
 
 const SubwayPage = () => {
   return (
     <s.Wrapper>
       <Header />
+      <s.CategoryContainer>
+        <s.Category_GANGNAM>강남역</s.Category_GANGNAM>
+        <s.Category_OTHERS>홍대입구역</s.Category_OTHERS>
+        <s.Category_OTHERS>신도림역</s.Category_OTHERS>
+        <s.Category_OTHERS>사당역</s.Category_OTHERS>
+      </s.CategoryContainer>
+      <s.SubwayImg src={subway} alt="subway" />
     </s.Wrapper>
   )
 }
