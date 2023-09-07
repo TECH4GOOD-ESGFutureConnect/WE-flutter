1fd08e6753afce05535d7bcfd086488864ca9987
sangjun-Park1208 <77184523+sangjun-Park1208@users.noreply.github.com>
2023-09-08 06:14:27 +0900

    feat: 메뉴 페이지 및 라우팅 완료

--- src/page/MenuPage/index.tsx
+++ src/page/MenuPage/index.tsx
@@ -1,10 +1,66 @@
 import Header from "../../component/Common/Header";
 import * as s from "./styles";
+import addIcon from "../../image/Menu/addIcon.svg";
+import busIcon from "../../image/Menu/busIcon.svg";
+import communityIcon from "../../image/Menu/communityIcon.svg";
+import convenientIcon from "../../image/Menu/convenientIcon.svg";
+import mapIcon from "../../image/Menu/mapIcon.svg";
+import {useNavigate} from "react-router-dom";
 
 const MenuPage = () => {
+  const navigate = useNavigate();
+
+  const onClickAdd = () => navigate('/');
+  const onClickCommunity = () => navigate('/community');
+  const onClickMap = () => navigate('/subway');
+  const onClickBus = () => navigate('/bus');
+  const onClickConvenient = () => navigate('/convenient');
+
   return (
     <s.Wrapper>
       <Header />
+      
+      <s.TabContainer>
+        <s.Tab onClick={onClickAdd}>
+          <s.TabIcon src={addIcon} alt="add"/>
+          <s.TabText>
+            <s.TabTitle>즐겨찾기</s.TabTitle>
+            <s.TabContent>내가 자주 가는 위험 지역 확인하기</s.TabContent>
+          </s.TabText>
+        </s.Tab>
+
+        <s.Tab onClick={onClickCommunity}>
+          <s.TabIcon src={busIcon} alt="bus"/>
+          <s.TabText>
+            <s.TabTitle>커뮤니티</s.TabTitle>
+            <s.TabContent>위험 정보 바로바로 알리기</s.TabContent>
+          </s.TabText>
+        </s.Tab>
+
+        <s.Tab onClick={onClickMap}>
+          <s.TabIcon src={communityIcon} alt="community"/>
+          <s.TabText>
+            <s.TabTitle>지하철 내부 이동 지도</s.TabTitle>
+            <s.TabContent>이동약자를 위한 지하철 내부 편의 지도</s.TabContent>
+          </s.TabText>
+        </s.Tab>
+
+        <s.Tab onClick={onClickBus}>
+          <s.TabIcon src={convenientIcon} alt="convenient"/>
+          <s.TabText>
+            <s.TabTitle>저상버스 예약</s.TabTitle>
+            <s.TabContent>손쉽게 저상버스를 예약</s.TabContent>
+          </s.TabText>
+        </s.Tab>
+
+        <s.Tab onClick={onClickConvenient}>
+          <s.TabIcon src={mapIcon} alt="map"/>
+          <s.TabText>
+            <s.TabTitle>편의시설 위치</s.TabTitle>
+            <s.TabContent>내 주변 위치 편의시설 알아보기</s.TabContent>
+          </s.TabText>
+        </s.Tab>
+      </s.TabContainer>
     </s.Wrapper>
   )
 }
--- src/page/MenuPage/styles.ts
+++ src/page/MenuPage/styles.ts
@@ -1,5 +1,58 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  width: 390px;
+  height: 100%;
+  background-color: #F1F2F6;
 `;
+
+export const TabContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Tab = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  width: 90%;
+  height: 80px;
+  margin-top: 10px;
+  background-color: white;
+  border-radius: 10px;
+`;
+
+export const TabIcon = styled.img`
+  display: flex;
+  width: 32px;
+  height: 32px;
+  margin-left: 12px;
+`;
+
+export const TabText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-left: 12px;
+  height: 50px
+`;
+
+export const TabTitle = styled.div`
+  display: flex;
+  font-size: 16px;
+`;
+
+export const TabContent = styled.div`
+  display: flex;
+  font-size: 12px;
+`;
+
+
+
+
